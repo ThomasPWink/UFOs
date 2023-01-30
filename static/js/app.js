9ec69df71aaf3e975c93850319ae53049477cc59
ThomasPWink <thomas.p.wink@gmail.com>
2023-01-30 14:53:24 -0700

    final module

--- static/js/app.js
+++ static/js/app.js
@@ -5,28 +5,31 @@ var tbody = d3.select("tbody");
 
 //build table
 function buildTable(data){
-tbody.html("");   
+    tbody.html("");   
 
-data.forEach((dataRow) => {
-    let row = tbody.append("tr");
-    Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+                cell.text(val);
 
+            }
+        );
     });
-});
 }
 
 // seach feature
 function handleClick () { 
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
-if (date) {
+
+
+    if (date) {
         filteredData = filteredData.filter(row => row.datetime === date);
-};
-buildTable(filteredData);
+    };
+    buildTable(filteredData);
+}
 
 d3.selectAll("#filter-btn").on("click", handleClick);
 
 buildTable(tableData);
-}
